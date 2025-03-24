646569e81a6b648d3a9ce72af4aa8c73055aa5bd
StestWu <d10319205@gmail.com>
2025-03-24 15:32:21 +0800

    1

--- tests/test_02_login.py
+++ tests/test_02_login.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import unittest
 import time
-from config.config import config  
+from config.config import Config,config  
 from utils.test_utils import CleanTextTestResult, CustomTextTestRunner
 from selenium.common.exceptions import TimeoutException
 from BaseTest import BaseTest
@@ -70,8 +70,8 @@ class LoginPageTest(BaseTest):
             phonenumber = self.wait.until(EC.presence_of_element_located((By.XPATH, "//input[@type='number']")))
             password = self.driver.find_element(By.XPATH, "//input[@type='password']")
             login_button = self.driver.find_element(By.XPATH, "//button[contains(text(), '登录')]")
-            phonenumber.send_keys(config.PHONE_NUMBER)
-            password.send_keys(config.VALID_PASSWORD)
+            phonenumber.send_keys(Config.PHONE_NUMBER)
+            password.send_keys(Config.VALID_PASSWORD)
             login_button.click()
 
             try:
@@ -92,7 +92,7 @@ class LoginPageTest(BaseTest):
             logger.debug("Verification code sent successfully")
 
             verify_code_input = self.wait.until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'input-group')]//input[@type='number' and @maxlength='6']")))
-            verify_code_input.send_keys(config.VERIFY_CODE)
+            verify_code_input.send_keys(Config.VERIFY_CODE)
 
             success_message = self.wait.until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '我的钱包')]")))
             self.assertIn("我的钱包", success_message.text)
@@ -116,7 +116,7 @@ class LoginPageTest(BaseTest):
             login_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), '登录')]")))
 
             # 使用無效的手機號碼
-            phonenumber.send_keys(config.generate_japanese_phone_number())  
+            phonenumber.send_keys(Config.generate_japanese_phone_number())  
             password.send_keys(config.VALID_PASSWORD)
             login_button.click()
 
@@ -188,8 +188,8 @@ class LoginPageTest(BaseTest):
             username = self.wait.until(EC.presence_of_element_located((By.XPATH, "//input[@maxlength='18']")))
             password = self.driver.find_element(By.XPATH, "//input[@type='password']")
             login_button = self.driver.find_element(By.XPATH, "//button[contains(text(), '登录')]")
-            username.send_keys(config.VALID_USERNAME)
-            password.send_keys(config.VALID_PASSWORD)
+            username.send_keys(Config.VALID_USERNAME)
+            password.send_keys(Config.VALID_PASSWORD)
             login_button.click()
 
             success_message = self.wait.until(EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '我的钱包')]")))
@@ -207,7 +207,7 @@ class LoginPageTest(BaseTest):
             password = self.driver.find_element(By.XPATH, "//input[@type='password']")
             login_button = self.driver.find_element(By.XPATH, "//button[contains(text(), '登录')]")
             # random_username = self.generate_random_username()
-            username.send_keys(config.generate_random_username())
+            username.send_keys(Config.generate_random_username())
             password.send_keys(config.VALID_PASSWORD)
             login_button.click()
 
@@ -253,7 +253,7 @@ class LoginPageTest(BaseTest):
             email = self.wait.until(EC.presence_of_element_located((By.XPATH, "//input[@type='text']")))
             password = self.driver.find_element(By.XPATH, "//input[@type='password']")
             login_button = self.driver.find_element(By.XPATH, "//button[contains(text(), '登录')]")
-            email.send_keys(config.generate_random_email())
+            email.send_keys(Config.generate_random_email())
             password.send_keys(config.VALID_PASSWORD)
             login_button.click()
 
