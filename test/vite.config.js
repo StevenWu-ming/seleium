c3d4972d350da2e58977ffc86c3769d83fe01154
StestWu <d10319205@gmail.com>
2025-04-12 21:32:28 +0800

    1

--- test/vite.config.js
+++ test/vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // 綁定所有網卡，讓區網設備能訪問
     port: 5173,      // 可自定其他 port，例如 3000、8080 等
-    strictPort: true // 如果 port 被占用會直接報錯，不會自動換 port（可選）
-  }
-})
+    strictPort: true, // 如果 port 被占用會直接報錯，不會自動換 port（可選）
+    base: './',
+    build: { outDir: 'dist'}
+  
+
+}})
+ 
